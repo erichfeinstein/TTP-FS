d60868a10f418c57cf2633b27392a803972c45da
Eric Feinstein <erichfeinstein@gmail.com>
2019-06-22 11:13:51 -0400

    passport setup

--- server/app.js
+++ server/app.js
@@ -1,13 +1,37 @@
 const body = require('body-parser');
 const path = require('path');
+const passport = require('passport');
 const express = require('express');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
+// parse application/x-www-form-urlencoded
+app.use(body.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(body.json());
+
+// passport registration
+passport.serializeUser((user, done) => done(null, user.id));
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.models.user.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get('/', function(req, res, next) {
   res.sendFile('index.html');
 });
+
+app.use('/auth', require('./auth'));
 
 const PORT = 8080;
 app.listen(PORT, function() {
---
+++ server/auth/index.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const User = require('../db/models/user');
+module.exports = router;
+
+router.post('/login', async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { email: req.body.email } });
+    if (!user) {
+      console.log('No such user found:', req.body.email);
+      res.status(404).send('Wrong username and/or password');
+    } else if (!user.correctPassword(req.body.password)) {
+      console.log('Incorrect password for user:', req.body.email);
+      res.status(401).send('Wrong username and/or password');
+    } else {
+      req.login(user, err => (err ? next(err) : res.json(user)));
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/signup', async (req, res, next) => {
+  try {
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    req.login(user, err => (err ? next(err) : res.json(user)));
+  } catch (err) {
+    if (err.name === 'SequelizeUniqueConstraintError') {
+      res.status(409).send('User already exists');
+    } else {
+      next(err);
+    }
+  }
+});
+
+router.post('/logout', (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.redirect('/');
+});
+
+router.get('/me', (req, res) => {
+  if (req.user) res.json(req.user);
+  else res.json({});
+});
