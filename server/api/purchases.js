a36c6cfeba2ac94e489c348781baf6f9ed140d63
Eric Feinstein <erichfeinstein@gmail.com>
2019-06-22 14:59:44 -0400

    Adjusted handling of invalid requests (bad ticker symbol)

--- server/api/purchases.js
+++ server/api/purchases.js
@@ -2,13 +2,20 @@ const router = require('express').Router();
 const { Purchase } = require('../db/models');
 module.exports = router;
 
+//IEX functions
+const { getPrice } = require('../iex');
+
 router.post('/', async (req, res, next) => {
   try {
     if (req.user) {
       const tickerSymbol = req.body.tickerSymbol;
       const numberOfShares = req.body.numberOfShares;
-      await Purchase.create({ tickerSymbol, numberOfShares });
-      res.status(201).send('added product');
+      const pricePurchasedAt = await getPrice(tickerSymbol);
+      //Ensure valid tickerSymbol
+      if (pricePurchasedAt < 0) throw new Error('Bad ticker symbol!');
+      //TODO Add req.user.id to Purchase creation
+      await Purchase.create({ tickerSymbol, numberOfShares, pricePurchasedAt });
+      res.status(201).send('Successfully purchased');
     } else {
       res.send('You must be signed in to purchase');
     }
--- server/iex/index.js
+++ server/iex/index.js
@@ -6,10 +6,15 @@ const token = process.env.TOKEN || localToken;
 const URL = 'https://cloud.iexapis.com';
 
 const getPrice = async tickerSymbol => {
-  const { data } = await axios.get(
-    URL + `/stable/stock/${tickerSymbol}/price?token=${token}`
-  );
-  return data;
+  try {
+    const { data } = await axios.get(
+      URL + `/stable/stock/${tickerSymbol}/price?token=${token}`
+    );
+    return data;
+  } catch (error) {
+    console.error('Error! Bad ticker symbol');
+    return -1;
+  }
 };
 
 module.exports = { getPrice };
